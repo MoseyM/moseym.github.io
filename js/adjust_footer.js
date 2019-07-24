3e27a68e48d424244790df1a183f3080901a143c
margaret <mmoseyee@gmail.com>
2019-07-24 09:11:41 -0500

    Removed Jekyll and made a Simple one page show

--- js/adjust_footer.js
+++
@@ -1,8 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", function (event) {
-    var element = document.getElementById('wrap');
-    var height = element.offsetHeight;
-    if (height < screen.height) {
-        document.getElementsByClassName("footer")[0].classList.add('stickybottom');
-    }
-}, false);
